9e77068f0439e406420c9f15cdfe9c3ec14228a2
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-09-01 16:36:05 +0200

    The real final save of the day for backup :)

--- src/components/navigation/navigationdesktop/NavigationDesktop.js
+++ src/components/navigation/navigationdesktop/NavigationDesktop.js
@@ -1,11 +1,12 @@
 import { useHistory } from 'react-router-dom'
 import RoutingPaths from '../../../routes/RoutingPaths'
 import { NavButton } from '../../navbutton/NavButton'
+import './NavigationDesktop.css'
 
 export const NavigationDesktop = () => {
     const history = useHistory()
     return (
-        <nav>
+        <nav className="nav-start">
             <NavButton buttonText="Home" path={RoutingPaths.home}/>
             <NavButton buttonText="About" path={RoutingPaths.about}/>
             <NavButton buttonText="Portfolio" path={RoutingPaths.portfolio}/>
--- src/routes/RoutingPaths.js
+++ src/routes/RoutingPaths.js
@@ -7,6 +7,6 @@ export default{
     fourOFour,
     home,
     about,
-    portfolio
+    portfolio,
 
 }
