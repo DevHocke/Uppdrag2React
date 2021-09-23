66ff93a539c2842563ddc5fdc50a79e2675df966
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-09-23 21:02:27 +0200

    Starting part 6 - useState

--- src/views/accessoriesview/AccessoriesView.js
+++ src/views/accessoriesview/AccessoriesView.js
@@ -4,13 +4,13 @@ import { useLocation } from 'react-router'
 
 export const AccessoriesView = () => {
     const [ServerResponse, setServerResponse] = useState();
-
+    
     const fetchData = async () => {
         try{
             const response = await axios.get("https://pokeapi.co/api/v2/pokemon/1")
             setServerResponse(response);
         } catch(error){
-            alert("Error retriving data from server pappskalle: " + error);
+            alert("Error retriving data from server: " + error);
         }
     };
 
@@ -19,7 +19,7 @@ export const AccessoriesView = () => {
             <main>
                 <section>
                     <h1>This is the Accessories view.</h1>
-                    <h2>{ServerResponse?.data?.name}</h2>
+                    <h2>{ServerResponse?.data?.species.name}</h2>
                     <button onClick={() => fetchData()}>Make API call</button>
                 </section>
             </main>
